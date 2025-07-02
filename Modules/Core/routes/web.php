d053120cfb646fe0fddfba177f5b37c857001002
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-07-02 11:23:00 +0600

    Dashboard Mastering partial

--- Modules/Core/routes/web.php
+++ Modules/Core/routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Core\Http\Controllers\CoreController;
+use Modules\Core\Http\Controllers\DashboardController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('cores', CoreController::class)->names('core');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('user.dashboard');
 });
