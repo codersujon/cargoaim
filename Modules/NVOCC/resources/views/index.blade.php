6b6234b1d2d318b3dcf935003f8e442fa370405f
MD. SUJAN AHMED <csesujon155@gmail.com>
2025-07-21 12:42:01 +0600

    Brand Name Issue

--- Modules/NVOCC/resources/views/index.blade.php
+++ Modules/NVOCC/resources/views/index.blade.php
@@ -2,25 +2,20 @@
 @section('title', "| {{ transText('nvocc_ch') }}")
 
 @section('content')
-
-    <div class="row pt-3">
-        <div class="col-lg-12">
-            <div class="card">
-
-                <div class="card-header border-bottom d-flex justify-content-between align-items-center">
-                    <h4 class="card-title mb-0" style="font-weight: 750;">{{ transText('nvocc_ch') }}</h4>
-                    <button type="button" class="btn btn-primary ms-1" id="createNew1">
-                        {{ transText('new_hbl_file_btn') }}
-                    </button>
-                </div>
-
-                <div class="card-body">
-                       
-                </div><!-- end card-body -->
-            </div><!-- end card -->
+<div class="row pt-2">
+    <div class="col-xl-12">
+        <div class="tabs-container">
+            <div class="card-header border-bottom border-dashed d-flex align-items-center">
+                <h4 class="header-title">{{ transText('nvocc_ch') }}</h4>
+            </div>
+             
         </div>
-        <!-- end col -->
-    </div>
-    <!-- end row -->
-@endsection
+    </div> <!-- end col -->
+</div>
 
+<style>
+
+</style>
+
+
+@endsection
